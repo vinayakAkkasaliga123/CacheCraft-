c2a09e8fbeb513e15aa1f6707239c576604c8b0a
vinayakAkkasaliga123 <akkasaligavinayak@gmail.com>
2025-11-07 20:34:43 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { cacheMiddleware } from "./cache/cacheMiddleware.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// API endpoint with caching middleware
+app.get("/api/products", cacheMiddleware, (req, res) => {
+  const dataPath = path.join("data", "products.json");
+  const products = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+
+  // simulate delay to show cache improvement
+  setTimeout(() => {
+    res.json({
+      success: true,
+      source: "database",
+      data: products
+    });
+  }, 2000);
+});
+
+// root
+app.get("/", (req, res) => {
+  res.send("Welcome to CacheCraft 🚀 - Custom Caching Layer for APIs");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
