cfba310eb66c0bc52b2676b7672d6ae3ebac1aec
vinayakAkkasaliga123 <akkasaligavinayak@gmail.com>
2025-11-07 21:39:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,6 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const dataPath = path.join(__dirname, "data", "products.json");
 
 
